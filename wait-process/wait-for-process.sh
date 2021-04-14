6373072b2c01e50307e0bbaf0d46612e43396705
raven4ever <wrtv23@gmail.com>
2021-04-14 09:09:43 +0300

    format + gitignore

--- wait-process/wait-for-process.sh
+++ wait-process/wait-for-process.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if ! kill -0 $1; then
-    echo "Process $1 doesn't exist."
-    exit 1
+  echo "Process $1 doesn't exist."
+  exit 1
 fi
 
 while kill -0 $1; do
-    echo "Still running!"
-    sleep 30
+  echo "Still running!"
+  sleep 30
 done
 
 echo "Process $1 finished at $(date)."
